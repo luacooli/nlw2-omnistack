3e1214646409fd18fb39183da09579ef895c9933
nakalua <luanadez13@gmail.com>
2020-08-12 08:43:50 -0300

    feat: server config and teacheForm style

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -5,7 +5,29 @@ import PageHeader from '../../components/PageHeader';
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrivel que voce quer dar aula." />
+      <PageHeader
+        title="Que incrivel que voce quer dar aula."
+        description="O primeiro passo e preencher esse formulario de inscricao"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus Dados</legend>
+
+          <div className="input-block">
+            <label htmlFor="name">Nome Completo</label>
+            <input type="text" id="name" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="avater">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   )
 }
